35a5210481df9d3d5b4981e2ab2beb718a702295
tiru-2001 <tirumaleshae709@gmail.com>
2023-06-23 22:54:52 +0530

    added multiple files

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,10 +1,46 @@
 import React from "react";
-import Home from "./Home";
+import "./login.scss";
+import login from "../images/login.jpg";
 
 const Login = () => {
   return (
     <>
-      <Home />
+      <div className="container-fluid">
+        <div className="row RegisterMainrow justify-content-center align-items-center ">
+          <div className="col-md-9 col__9">
+            <h1 className="text-center registerheading">Login</h1>
+            <div className="row justify-content-center align-items-center secondRow">
+              <div className="col-md-4 imagecontainer">
+                <img className="image-fluid" src={login} alt="" />
+              </div>
+              <div className="col-md-4 registercontainer ">
+                <form action="">
+                  <label htmlFor="email">Email:</label>
+                  <br />
+                  <input
+                    placeholder="Enter the email"
+                    id="email"
+                    type="email"
+                  />
+                  <br />
+                  <label htmlFor="pass">Password:</label>
+                  <br />
+                  <input
+                    placeholder="Enter the password"
+                    id="pass"
+                    type="password"
+                  />
+                  <br />
+
+                  <button type="button" class="btn btn-outline-primary">
+                    Login
+                  </button>
+                </form>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   );
 };
--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,7 +1,59 @@
 import React from "react";
+import "./register.scss";
+import register from "../images/register.jpg";
 
 const Register = () => {
-  return <div>Registerjsdlkfjaslkdjflkasjdflkasjflkj</div>;
+  return (
+    <>
+      <div className="container-fluid">
+        <div className="row RegisterMainrow justify-content-center align-items-center ">
+          <div className="col-md-9 col__9">
+            <h1 className="text-center registerheading">Register</h1>
+            <div className="row justify-content-center align-items-center secondRow">
+              <div className="col-md-4 imagecontainer">
+                <img className="image-fluid" src={register} alt="" />
+              </div>
+              <div className="col-md-4 registercontainer ">
+                <form action="">
+                  <label htmlFor="name">Name:</label>
+                  <br />
+                  <input placeholder="Enter the name" id="name" type="text" />
+                  <br />
+                  <label htmlFor="email">Email:</label>
+                  <br />
+                  <input
+                    placeholder="Enter the email"
+                    id="email"
+                    type="email"
+                  />
+                  <br />
+                  <label htmlFor="pass">Password:</label>
+                  <br />
+                  <input
+                    placeholder="Enter the password"
+                    id="pass"
+                    type="password"
+                  />
+                  <br />
+                  <label htmlFor="phone">Phone:</label>
+                  <br />
+                  <input
+                    placeholder="Enter the number"
+                    id="phone"
+                    type="number"
+                  />
+                  <br />
+                  <button type="button" class="btn btn-outline-primary">
+                    submit
+                  </button>
+                </form>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Register;
